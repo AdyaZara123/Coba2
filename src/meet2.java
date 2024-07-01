6b30f682f643a51884acc772e7467c17b71bcdb2
AdyaAisyaZara <adya.22315042@umkendari.ac.id>
2024-07-02 05:43:58 +0700

    wkwkkw

---
+++ src/meet2.java
@@ -0,0 +1,9 @@
+public class meet2 {
+    public static void main(String[] args){
+        long[] arrayLong = {
+                10, 90, 80, 29
+        };
+        arrayLong[2] = 100;
+        System.out.println(arrayLong[2]);
+    }
+}
---
+++ src/meet3.java
@@ -0,0 +1,11 @@
+public class meet3 {
+    public static void main(String[] args){
+        int c = 100;
+        int b = 10;
+        c += 10;
+        b -= 100;
+
+        System.out.println(c);
+    }
+}
+
